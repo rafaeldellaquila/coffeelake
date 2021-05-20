56499081771250616f647987bd951baeb71245b0
rafaeldellaquila <rafadellaquila@gmail.com>
2021-05-19 21:29:47 -0300

    :truck: add basic routes

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import colors from '../styles/colors';
+import { Welcome } from '../pages/Welcome';
+
+const stackRoutes = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <stackRoutes.Navigator
+    headerMode='none'
+    screenOptions={{
+      cardStyle: {
+        backgroundColor: colors.background,
+      },
+    }}
+  >
+    <stackRoutes.Screen name='Welcome' component={Welcome} />
+  </stackRoutes.Navigator>
+);
+
+export default AppRoutes;
